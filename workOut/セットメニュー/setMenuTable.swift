8efe2590530449f90475f3618e75f7d7f32cad3a
笹倉一也 <>
2019-01-08 18:22:41 +0900

    tableview data done,,,

--- workOut/セットメニュー/setMenuTable.swift
+++ workOut/セットメニュー/setMenuTable.swift
@@ -9,15 +9,50 @@
 import UIKit
 import RealmSwift
 
-class setMenuTable: UIViewController {
+class setMenuTable: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var setMenuItem: Results<SetMenu>!
+    
+    @IBOutlet weak var setMenuTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let realm = try! Realm()
-    print(Realm.Configuration.defaultConfiguration.fileURL!)
+        setMenuItem = realm.objects(SetMenu.self).sorted(byKeyPath: "order")
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setMenuTableView.reloadData()
+    
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int {
+        
+        return setMenuItem.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexpath: IndexPath)->UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        
+        let object = setMenuItem[indexpath.row]
+        
+        cell.textLabel?.text = object.setName
+        
+        return cell
         
         
     }
+    
+    
+    
+    
+    
   
 }
