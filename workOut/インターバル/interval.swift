aaebfcfcc1457e33ec03a99cfbe01f0024c1df07
笹倉一也 <>
2019-01-12 14:31:59 +0900

    next interval.....

---
+++ workOut/インターバル/interval.swift
@@ -0,0 +1,23 @@
+//
+//  interval.swift
+//  workOut
+//
+//  Created by 笹倉一也 on 2019/01/12.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class interval: UIViewController {
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
