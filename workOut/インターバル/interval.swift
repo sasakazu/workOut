a0cec00b6e4e514a4907382ad4e882bfe242a796
笹倉一也 <>
2019-01-20 14:06:47 +0900

    interval layout change...

--- workOut/インターバル/interval.swift
+++ workOut/インターバル/interval.swift
@@ -28,6 +28,36 @@ class interval: UIViewController {
         startButton.layer.cornerRadius = 5.0
         stopButton.layer.cornerRadius = 5.0
         
+        //        ぼかし
+        timerMinute.layer.shadowColor = UIColor.black.cgColor
+        timerMinute.layer.shadowOpacity = 0.5
+        timerMinute.layer.shadowOffset = CGSize(width: 3, height: 3)
+        timerMinute.layer.shadowRadius = 5
+        
+        //        ぼかし
+        timerSecond
+            .layer.shadowColor = UIColor.black.cgColor
+        timerSecond.layer.shadowOpacity = 0.5
+        timerSecond.layer.shadowOffset = CGSize(width: 3, height: 3)
+        timerSecond.layer.shadowRadius = 5
+        
+        //        ぼかし
+        timerMSec.layer.shadowColor = UIColor.black.cgColor
+        timerMSec.layer.shadowOpacity = 0.5
+        timerMSec.layer.shadowOffset = CGSize(width: 3, height: 3)
+        timerMSec.layer.shadowRadius = 5
+        
+//        ぼかし
+        startButton.layer.shadowColor = UIColor.black.cgColor
+        startButton.layer.shadowOpacity = 0.5
+        startButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        startButton.layer.shadowRadius = 5
+  
+//        ぼかし
+        stopButton.layer.shadowColor = UIColor.black.cgColor
+        stopButton.layer.shadowOpacity = 0.5
+        stopButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        stopButton.layer.shadowRadius = 5
 
     }
     
